4958347125c4a93d1266b9675d532b6fbb483a44
Daniel Chaves <danielTchaves@hotmail.com>
2020-06-24 23:34:53 -0300

    Adicionar arquivos de projeto.

---
+++ MVVM.Exemplo/MVVM.Exemplo/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MVVM.Exemplo.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM.Exemplo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new BindingModePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MVVM.Exemplo/MVVM.Exemplo/ViewModels/BindingModeViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM.Exemplo.ViewModels
+{
+    public class BindingModeViewModel : BaseViewModel
+    {
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private string _sobreNome;
+        public string SobreNome
+        {
+            get { return _sobreNome; }
+            set
+            {
+                _sobreNome = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _endereco;
+        public string Endereco
+        {
+            get { return _endereco; }
+            set
+            {
+                _endereco = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ICommand _limparCommand;
+        public ICommand LimparCommand
+        {
+            get
+            {
+                return _limparCommand ?? new Command(() =>
+               {
+                   Endereco = string.Empty;
+               });
+            }
+            set
+            {
+                _limparCommand = value;
+            }
+        }
+
+        public BindingModeViewModel()
+        {
+            Nome = "Daniel";
+            SobreNome = "Teixeira";
+        }
+    }
+}
