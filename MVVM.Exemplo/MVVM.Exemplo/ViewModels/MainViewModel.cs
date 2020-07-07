62afdb204ae2aae2eae92853bbe35ed55a78c63a
Daniel Chaves <danielTchaves@hotmail.com>
2020-07-07 20:52:08 -0300

    ok

--- MVVM.Exemplo/MVVM.Exemplo/ViewModels/MainViewModel.cs
+++ MVVM.Exemplo/MVVM.Exemplo/ViewModels/MainViewModel.cs
@@ -1,5 +1,8 @@
-﻿using MVVM.Exemplo.ViewModels.Base;
+﻿using MVVM.Exemplo.Models;
+using MVVM.Exemplo.ViewModels.Base;
 using MVVM.Exemplo.ViewModels.Interfaces;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -7,6 +10,20 @@ namespace MVVM.Exemplo.ViewModels
 {
     public class MainViewModel : BaseViewModel, IMainViewModel
     {
+        public ObservableCollection<Usuario> Usuarios { get; set; }
+
+        private ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get
+            {
+                return _addCommand ?? (_addCommand = new Command(() =>
+                {
+                    Usuarios.Add(new Usuario() { Nome = "Usuario", SobreNome = "Teste"});
+                }));
+            }
+        }
+
         private ICommand _tapCommand;
         public ICommand TapCommand
         {
@@ -17,6 +34,11 @@ namespace MVVM.Exemplo.ViewModels
 
                 }));
             }
+        }
+
+        public MainViewModel()
+        {
+            Usuarios = new ObservableCollection<Usuario>();
         }
     }
 }
